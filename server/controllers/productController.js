211e2c4899a7e4ffb5d68ee8d1bfdf185f4ab994
Anuja-Fernando <anujafernando35@gmail.com>
2026-06-16 12:11:16 +0530

    Add files via upload

---
+++ server/controllers/productController.js
@@ -0,0 +1,81 @@
+const Product = require('../models/productModel')
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncError = require('../middlewares/catchAsyncError')
+const APIFeatures = require('../utils/apiFeatures')
+
+//Get Products - /api/v1/products
+exports.getProducts = async(req,res,next) =>{
+    const resPerPage = 2;
+    const apiFeatures = new APIFeatures(Product.find(),req.query).search().filter().paginate(resPerPage);
+
+    const products = await apiFeatures.query;
+    res.status(200).json({
+        success:true,
+        count:products.length,
+        products
+    })
+}
+
+//Create Product - /api/v1/product/new
+exports.newProduct = catchAsyncError(async (req,res,next) =>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+})
+
+//Get Single Product - {{base_url}}/api/v1/product/6a2c1c5d4163b309354eccf2
+exports.getSingleProduct = async(req,res,next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHandler('Product not found',400));
+    }
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+//Update Product - {{base_url}}/api/v1/product/6a2c1c5d4163b309354eccf2
+exports.updateProduct = async(req,res,next) =>{
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Delete Product
+exports.deleteProduct = async(req,res,next) =>{
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+    
+    res.status(200).json({
+        success: true,
+        message: "Products Deleted"
+    })
+}
---
+++ server/middlewares/authenticate.js
@@ -0,0 +1,17 @@
+const catchAsyncError = require("./catchAsyncError");
+const User = require('../models/userModel')
+const catchAsyncError = require('./catchAsyncError');
+const jwt = require('jsonwebtoken');
+const ErrorHandler = require("../utils/errorHandler");
+
+exports.isAuthenticatedUser = catchAsyncError(async (req,res,next) =>{
+    const {token} = req.cookies;
+
+    if(!token){
+        return next(new ErrorHandler('Login first to handle this resource',401));
+    }
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+})
